19ecbad5e04db49e83a07e3e33362651994f9897
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-05-10 19:28:04 +0000

    GET y POST de People y Planets con bonus

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,84 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    # obtiene todos los objetos de usuario de la base de datos
+    users = User.query.all()
 
-    return jsonify(response_body), 200
+    # crea una lista de diccionarios con la información de cada usuario
+    user_list = [
+        {
+            "id": user.id,
+            "email": user.email
+            # agrega aquí cualquier otra información que quieras devolver
+        }
+        for user in users
+    ]
+
+    return jsonify(user_list), 200
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    allPlanets = Planet.query.all()
+    result = [element.serialize() for element in allPlanets]
+    return jsonify(result), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_single_planet(planet_id):
+    planet1 = Planet.query.get(planet_id)
+    if planet1 == None : 
+        return 'Planeta no encontrado', 404
+    return jsonify(planet1.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+
+    # obtain the JSON data from the request and convert it to a Python dictionary
+    data = request.get_json()
+
+    # create a new Planet object with the provided data
+    planet = Planet(name=data['name'], description=data['description'], population=data['population'])
+
+    # add the new planet to the database
+    db.session.add(planet)
+    db.session.commit()
+
+    # Serialize the inserted planet data
+    inserted_planet_data = planet.serialize()
+
+    return jsonify(inserted_planet_data), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    allPeople = People.query.all()
+    result = [element.serialize() for element in allPeople]
+    return jsonify(result),200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_single_people(people_id):
+    people1 = People.query.get(people_id)
+    if people1 == None : 
+        return 'Persona no encontrada', 404
+    return jsonify(people1.serialize()), 200
+
+@app.route('/people', methods=['POST'])
+def post_people():
+
+    # obtain the JSON data from the request and convert it to a Python dictionary
+    data = request.get_json()
+
+    # create a new People object with the provided data
+    people = People(name=data['name'], description=data['description'], gender=data['gender'], hair_color=data['hair_color'])
+
+    # add the new person to the database
+    db.session.add(people)
+    db.session.commit()
+
+    # Serialize the inserted person data
+    inserted_person_data = people.serialize()
+
+    return jsonify(inserted_person_data), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
